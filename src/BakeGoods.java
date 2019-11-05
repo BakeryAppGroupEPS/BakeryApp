60da6bfbf0027b09f31dab773f68cea1d68c7064
escobar55 <escobar5@umbc.edu>
2019-11-05 10:36:15 -0500

    from1to5

--- src/BakeGoods.java
+++ src/BakeGoods.java
@@ -6,11 +6,11 @@ public class BakeGoods {
     public static void main(String[] args) {
 
         //Create 15 bake goods
-        Main bakeGood1 = new Main("naked berry", );
-        Main bakeGood2 = new Main();
-        Main bakeGood3 = new Main();
-        Main bakeGood4 = new Main();
-        Main bakeGood5 = new Main();
+        Main bakeGood1 = new Main("naked berry", 27.00, "11/06/19", "gluten, dairy, eggs", 1);
+        Main bakeGood2 = new Main("banana bread", 5.95, "11/06/19", "soy, tree nuts, sesame", 5);
+        Main bakeGood3 = new Main("multi grain white bread", 10.95, "11/11/19", "eggs,say,tree nuts", 5);
+        Main bakeGood4 = new Main("blueberry6-pack of muffins", 16.50, "11/11/19", "sesame, dairy, soy", 3);
+        Main bakeGood5 = new Main("large hamburger buns", 5.00, "11/11/19", "sesame, gluten, dairy", 12);
         Main bakeGood6 = new Main();
         Main bakeGood7 = new Main();
         Main bakeGood8 = new Main();
