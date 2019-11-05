9419b70b15e12a58e08603d59c07feed8ad25241
escobar55 <escobar5@umbc.edu>
2019-11-05 15:58:51 -0500

    final bakeryApp

--- src/BakeryDB.java
+++ src/BakeryDB.java
@@ -13,69 +13,35 @@ public class BakeryDB {
     public void setBakesDB(ArrayList<BakeGoods> bakesDB) {
         this.bakesDB = bakesDB;
     }
+
+    //Array method
+    public ArrayList<BakeGoods> check(String answer){
+        boolean checker;
+        ArrayList<BakeGoods> matched = new ArrayList<>();
+
+            for(BakeGoods b : bakesDB) {
+                checker = false;
+               for(String d : b.getDietRestriction()){
+                   if(d.equalsIgnoreCase(answer)){
+                       checker = true;
+                       break;
+
+                   }
+               }
+               if (checker == false){
+                   matched.add(b);
+               }
+
+            }
+            return matched;
+    }
+
+    public void print(ArrayList<BakeGoods> matched){
+        for(BakeGoods c : matched){
+            c.displayText();
+        }
+    }
+
+
 }
 
-/*
-
-
-name - naked berry
-price - 27.00
-expiration date - 11/06/19
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
-quantity- 1
-
-name - banana bread
-price - 5.95
-expiration date - 11/06/19
-diet restrictions - gluten, dairy, eggs, soy, tree nuts, sesame
-quantity - 5
-
-name - multi grain white bread
-price - 10.95
-expiration date - 11/11/19
-diet restrictions -gluten, dairy, eggs, soy, tree nuts, sesame
-quantity - 5
-
-name - blueberry6-pack of muffins
-price - 16.50
-expiration date - 11/11/19
-diet restrictions - gluten, sesame, dairy, soy, eggs, tree nuts
-quantity - 3
-
-name - large hamburger buns
-price - 5.00
-expiration date - 11/11/19
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
-quantity - 12
-
-name - cinnamon bread
-price - 10.95
-expiration date - 11/11/19
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
-quantity - 2
-
-name - soft pizza crust
-price - 6.95
-expiration date - 1/16/20
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame,soy
-quantity - 5
-
-name - dozen savory empanadas
-price - 39.00
-expiration date - 11/06/19
-diet restrictions - gluten, dairy, eggs, soy, peanuts, treenuts, sesame
-quantity - 4
-
-name - peppermint velvet roll
-price - 36.95
-expiration date - 11/06/19
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
-quantity - 1
-
-name - rosette one cake
-price - 32.00
-expiration date 11/06/2019
-diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
-quantity - 1
-
- */
--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,30 @@ public class Main {
         bakeGood.displayWelcomeText(); //display welcome message
 
         //Create 15 bake goods
-        BakeGoods bakeGood1 = new BakeGoods("Naked berry", 27.00, "11/06/19", "gluten, dairy, eggs", 1);
-        BakeGoods bakeGood2 = new BakeGoods("Banana bread", 5.95, "11/06/19", "soy, tree nuts, sesame", 5);
-        BakeGoods bakeGood3 = new BakeGoods("Multi grain white bread", 10.95, "11/11/19", "eggs,say,tree nuts", 5);
-        BakeGoods bakeGood4 = new BakeGoods("Blueberry6-pack of muffins", 16.50, "11/11/19", "sesame, dairy, soy", 3);
-        BakeGoods bakeGood5 = new BakeGoods("Large hamburger buns", 5.00, "11/11/19", "sesame, gluten, dairy", 12);
-        BakeGoods bakeGood6 = new BakeGoods("Cinnamon Bread", 10.95, "11/19/19", "gluten, dairy, eggs", 2);
-        BakeGoods bakeGood7 = new BakeGoods("Soft Crust Pizza", 6.95, "1/16/20", "peanuts, tree nuts, sesame", 5);
-        BakeGoods bakeGood8 = new BakeGoods("Dozen Savory Empanadas", 39.00, "11/06/19", "sesame, gluten, dairy", 4);
-        BakeGoods bakeGood9 = new BakeGoods("Peppermint Velvet Roll", 36.95, "11/06/19", "tree nuts, peanuts, eggs", 1);
-        BakeGoods bakeGood10 = new BakeGoods("Rosette One Cake", 32.00, "11/06/19", "gluten, eggs, sesame", 1);
-        BakeGoods bakeGood11 = new BakeGoods("Golden flower cake", 64.00, "11/06/19", "gluten , dairy, eggs", 1);
-        BakeGoods bakeGood12 = new BakeGoods("3 ghost cake", 32.00, "11/06/19", "gluten , dairy, eggs", 2);
-        BakeGoods bakeGood13 = new BakeGoods("Casino cake", 54.00, "11/08/19", "gluten , dairy, eggs", 1);
-        BakeGoods bakeGood14 = new BakeGoods("Little mermaid cake", 25.00, "11/06/19", "gluten , dairy, eggs", 3);
-        BakeGoods bakeGood15 = new BakeGoods("Soccer ball cake", 49.00, "11/06/19", "gluten , dairy, eggs", 2);
+        BakeGoods bakeGood1 = new BakeGoods("Naked berry", 27.00, "11/06/19", new String[]{"gluten", "dairy", "eggs"}, 1);
+        BakeGoods bakeGood2 = new BakeGoods("Banana bread", 5.95, "11/06/19",  new String[]{"soy", "tree nuts", "sesame"}, 5);
+        BakeGoods bakeGood3 = new BakeGoods("Multi grain white bread", 10.95, "11/11/19",  new String[]{"eggs","soy","tree nuts"}, 5);
+        BakeGoods bakeGood4 = new BakeGoods("Blueberry6-pack of muffins", 16.50, "11/11/19",  new String[]{"sesame", "dairy", "soy"}, 3);
+        BakeGoods bakeGood5 = new BakeGoods("Large hamburger buns", 5.00, "11/11/19",  new String[]{"sesame", "gluten", "dairy"}, 12);
+        BakeGoods bakeGood6 = new BakeGoods("Cinnamon Bread", 10.95, "11/19/19", new String[]{"gluten"}, 2);
+        BakeGoods bakeGood7 = new BakeGoods("Soft Crust Pizza", 6.95, "1/16/20", new String[]{"peanuts", "tree nuts", "sesame"}, 5);
+        BakeGoods bakeGood8 = new BakeGoods("Dozen Savory Empanadas", 39.00, "11/06/19", new String[]{"sesame", "gluten", "dairy"}, 4);
+        BakeGoods bakeGood9 = new BakeGoods("Peppermint Velvet Roll", 36.95, "11/06/19", new String[]{"tree nuts"}, 1);
+        BakeGoods bakeGood10 = new BakeGoods("Rosette One Cake", 32.00, "11/06/19", new String[]{"gluten", "eggs"}, 1);
+        BakeGoods bakeGood11 = new BakeGoods("Golden flower cake", 64.00, "11/06/19",new String[]{"gluten" , "dairy"}, 1);
+        BakeGoods bakeGood12 = new BakeGoods("3 ghost cake", 32.00, "11/06/19", new String[]{"gluten" , "dairy", "eggs"}, 2);
+        BakeGoods bakeGood13 = new BakeGoods("Casino cake", 54.00, "11/08/19", new String[]{"gluten" , "dairy", "eggs"}, 1);
+        BakeGoods bakeGood14 = new BakeGoods("Little mermaid cake", 25.00, "11/06/19", new String[]{"gluten" , "dairy"}, 3);
+        BakeGoods bakeGood15 = new BakeGoods("Soccer ball cake", 49.00, "11/06/19", new String[]{"eggs"}, 2);
+
+        //Ask the user for diet restrictions
+        Scanner key = new Scanner(System.in);
+        System.out.println();
+        System.out.println("To search for foods that you can eat, please enter the name of the diet restriction: ");
+        System.out.println("To see a list of all our foods, please type all");
+        String answer = key.next();
+
+
 
         //********** Calls the arrayList from the BakeryDB class *******************//
         ArrayList<BakeGoods> bakesArrayList = new ArrayList<>();
@@ -32,15 +41,20 @@ public class Main {
                 , bakeGood13, bakeGood14, bakeGood15);
         BakeryDB bakesDB = new BakeryDB();
         bakesDB.setBakesDB(bakesArrayList);
-        for (BakeGoods b : bakesDB.getBakesDB()){
-            b.displayText(); //still waiting for method to be created
+
+        if(answer.equalsIgnoreCase("all")) {
+      for (BakeGoods b : bakesDB.getBakesDB()){
+           b.displayText(); //still waiting for method to be created
+       }
         }
+
         //***********************************************************************//
+        ArrayList<BakeGoods> x = bakesDB.check(answer);
+        bakesDB.print(x);
 
-        //Ask the user for diet restrictions
 
-        Scanner key = new Scanner(System.in);
-        String answer = key.nextLine();
+
+
 
     }
 }
