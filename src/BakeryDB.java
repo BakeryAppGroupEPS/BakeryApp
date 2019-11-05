1d7524213a3258f6818dbb2f9be0e61c5127796c
EfremBAK <efrem.berhane2009@gmail.com>
2019-11-05 10:16:59 -0500

    list

--- src/BakeryDB.java
+++ src/BakeryDB.java
@@ -14,3 +14,68 @@ public class BakeryDB {
         this.bakesDB = bakesDB;
     }
 }
+
+/*
+
+
+name - naked berry
+price - 27.00
+expiration date - 11/06/19
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
+quantity- 1
+
+name - banana bread
+price - 5.95
+expiration date - 11/06/19
+diet restrictions - gluten, dairy, eggs, soy, tree nuts, sesame
+quantity - 5
+
+name - multi grain white bread
+price - 10.95
+expiration date - 11/11/19
+diet restrictions -gluten, dairy, eggs,say,tree nuts, sesame
+quantity - 5
+
+name - blueberry6-pack of muffins
+price - 16.50
+expiration date - 11/11/19
+diet restrictions - gluten, sesame, dairy, soy, eggs, tree nuts
+quantity - 3
+
+name - large hamburger buns
+price - 5.00
+expiration date - 11/11/19
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
+quantity - 12
+
+name - cinnamon bread
+price - 10.95
+expiration date - 11/11/19
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
+quantity - 2
+
+name - soft pizza crust
+price - 6.95
+expiration date - 1/16/20
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame,soy
+quantity - 5
+
+name - dozen savory empanadas
+price - 39.00
+expiration date - 11/06/19
+diet restrictions - gluten, dairy, eggs, soy, peanuts, treenuts, sesame
+quantity - 4
+
+name - peppermint velvet roll
+price - 36.95
+expiration date - 11/06/19
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
+quantity - 1
+
+name - rosette one cake
+price - 32.00
+expiration date 11/06/2019
+diet restrictions - gluten, dairy, eggs, peanuts, treenuts, sesame
+quantity - 1
+
+ */
