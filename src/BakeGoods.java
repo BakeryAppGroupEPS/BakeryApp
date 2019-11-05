755fa7b98e0dcc77f30bb9f317d9512f72539b47
EfremBAK <efrem.berhane2009@gmail.com>
2019-11-05 11:07:39 -0500

    trial

--- src/BakeGoods.java
+++ src/BakeGoods.java
@@ -6,21 +6,21 @@ public class BakeGoods {
     public static void main(String[] args) {
 
         //Create 15 bake goods
-        Main bakeGood1 = new Main("naked berry", 27.00, "11/06/19", "gluten, dairy, eggs", 1);
-        Main bakeGood2 = new Main("banana bread", 5.95, "11/06/19", "soy, tree nuts, sesame", 5);
-        Main bakeGood3 = new Main("multi grain white bread", 10.95, "11/11/19", "eggs,say,tree nuts", 5);
-        Main bakeGood4 = new Main("blueberry6-pack of muffins", 16.50, "11/11/19", "sesame, dairy, soy", 3);
-        Main bakeGood5 = new Main("large hamburger buns", 5.00, "11/11/19", "sesame, gluten, dairy", 12);
-        Main bakeGood6 = new Main("Cinnamon Bread", 10.95, "11/19/19", "gluten, dairy, eggs", 2);
-        Main bakeGood7 = new Main("Soft Crust Pizza", 6.95, "1/16/20", "peanuts, tree nuts, sesame", 5);
-        Main bakeGood8 = new Main("Dozen Savory Empanadas", 39.00, "11/06/19", "sesame, gluten, dairy", 4);
-        Main bakeGood9 = new Main("Peppermint Velvet Roll", 36.95, "11/06/19", "tree nuts, peanuts, eggs", 1);
-        Main bakeGood10 = new Main("Rosette One Cake", 32.00, "11/06/19", "gluten, eggs, sesame", 1);
-        Main bakeGood11 = new Main();
-        Main bakeGood12 = new Main();
-        Main bakeGood13 = new Main();
-        Main bakeGood14 = new Main();
-        Main bakeGood15 = new Main();
+        Main bakeGood1 = new Main("naked berry", );
+        Main bakeGood2 = new Main();
+        Main bakeGood3 = new Main();
+        Main bakeGood4 = new Main();
+        Main bakeGood5 = new Main();
+        Main bakeGood6 = new Main();
+        Main bakeGood7 = new Main();
+        Main bakeGood8 = new Main();
+        Main bakeGood9 = new Main();
+        Main bakeGood10 = new Main();
+        Main bakeGood11 = new Main("golden flower cake", 64.00, "11/06/19", "gluten , dairy, eggs", 1);
+        Main bakeGood12 = new Main("3 ghost cake", 32.00, "11/06/19", "gluten , dairy, eggs", 2);
+        Main bakeGood13 = new Main("casino cake", 54.00, "11/08/19", "gluten , dairy, eggs", 1);
+        Main bakeGood14 = new Main("little mermaid cake", 25.00, "11/06/19", "gluten , dairy, eggs", 3);
+        Main bakeGood15 = new Main("soccer ball cake", 49.00, "11/06/19", "gluten , dairy, eggs", 2);
 
         //********** Calls the arrayList from the BakeryDB class *******************//
         ArrayList<Main> bakesArrayList = new ArrayList<>();
@@ -35,6 +35,7 @@ public class BakeGoods {
         //***********************************************************************//
 
         //Ask the user for diet restrictions
+
         Scanner key = new Scanner(System.in);
         System.out.println("Welcome to the Brookley's Better Bakery App! \n" +
                 "We contain peanut, gluten, soy and dairy in our products. \n");
